ef8707133d24a6d034a426edfa3e9a668442f7cc
sudonnoh <sudonnoh@gmail.com>
2022-03-18 15:16:13 +0900

    update

--- authentication/backends.py
+++ authentication/backends.py
@@ -25,7 +25,6 @@ class JWTAuthentication(authentication.BaseAuthentication):
             error가 발생한 경우 어떤 것도 반환하지 않습니다. 단지 'AuthenticationFailed' 
             error를 보내고, 나머지는 DRF가 처리하도록 합니다.
         """
-        # request.user = None
 
         # 'auth_header'는 두 가지 요소(element)를 배열로 갖고 있어야 합니다.
         # 1) authentication header의 이름(여기에서는 'Token')
@@ -46,9 +45,12 @@ class JWTAuthentication(authentication.BaseAuthentication):
         elif len(auth_header) > 2:
             return None
 
-        # prefix.lower는 우리가 바깥에서 받아온 'token' 값 입니다. postman에서 토큰값
+        # prefix.lower()는 우리가 바깥에서 받아온 'token' 값 입니다. postman에서 토큰값
         # 앞에 붙이 그 'token'입니다. 만약 받아온 'token'값이 저희가 앞서 설정한
         # auth_header_prefix의 값과 다르면 None을 반환합니다.
+        
+        # 여기서 auth_header_prefix는 class가 시작되는 초기에 설정한 authentication_header_prefix를
+        # 소문자로 바꾼 값입니다.
         
         # 한편 앞서 lower() 메소드를 이용해 전부 소문자로 바꾸었기 때문에 'Token'값이
         # 'token'값이 돼서 오류없이 통과합니다.
